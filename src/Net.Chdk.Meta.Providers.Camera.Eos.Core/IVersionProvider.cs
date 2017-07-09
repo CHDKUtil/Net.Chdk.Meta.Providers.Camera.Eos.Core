00689b3d2371a9b02055c71e0fe262ec6afc2544
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 06:22:17 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Camera.Eos.Core/IVersionProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.Camera.Eos;
+using Net.Chdk.Meta.Model.CameraList;
+using Net.Chdk.Meta.Model.CameraTree;
+
+namespace Net.Chdk.Meta.Providers.Camera.Eos
+{
+    public interface IVersionProvider
+    {
+        VersionData GetVersion(string version, TreeRevisionData treeRevision, ListPlatformData list, TreePlatformData tree);
+    }
+}
